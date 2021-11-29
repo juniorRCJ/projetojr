d44d107accc2711c8a7fcdf69de1df090688bc10
juniorRCJ <caldeira.junior@gmail.com>
2021-11-29 11:56:07 -0300

    projeto criado

---
+++ src/main/java/com/caldeira/projetojr/ProjetojrApplication.java
@@ -0,0 +1,13 @@
+package com.caldeira.projetojr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetojrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetojrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caldeira/projetojr/ProjetojrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caldeira.projetojr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetojrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
