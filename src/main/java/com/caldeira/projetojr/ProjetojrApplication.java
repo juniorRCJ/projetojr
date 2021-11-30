4e8ef13d839e122897eb7edef39ee8175c455a11
juniorRCJ <caldeira.junior@gmail.com>
2021-11-30 16:59:06 -0300

    Criando operação de instanciação

--- src/main/java/com/caldeira/projetojr/ProjetojrApplication.java
+++ src/main/java/com/caldeira/projetojr/ProjetojrApplication.java
@@ -1,13 +1,32 @@
 package com.caldeira.projetojr;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.caldeira.projetojr.domain.Categoria;
+import com.caldeira.projetojr.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ProjetojrApplication {
+public class ProjetojrApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetojrApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
